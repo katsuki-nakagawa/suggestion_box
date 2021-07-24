d50988b12165e3299bd4dde45f830766bff7b1b8
NakagawaKatsuki <ktk_0125@yahoo.co.jp>
2021-07-24 10:12:39 +0900

    feat: vue-routerをinstallして設定

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+import router from './router';
 
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/suggestion-box-dc26e/databases/(default)/documents"
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
