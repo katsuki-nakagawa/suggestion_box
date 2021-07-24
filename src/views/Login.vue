bbc84e679188f4405a25b080ddc8b0f16100d95c
NakagawaKatsuki <ktk_0125@yahoo.co.jp>
2021-07-25 07:48:21 +0900

    feat: ユーザー登録を実装

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,10 +2,10 @@
   <div>
     <h3>ログイン</h3>
     <label for="email">Email:</label>
-    <input id="email" type="email" v-model="name">
+    <input id="email" type="email" v-model="email">
     <br><br>
     <label for="password">パスワード:</label>
-    <input id="password" type="password" v-model="name">
+    <input id="password" type="password" v-model="password">
     <br><br>
     <button @click="login">送信</button>
   
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,10 +2,10 @@
   <div>
     <h3>登録</h3>
     <label for="email">Email:</label>
-    <input id="email" type="email" v-model="name">
+    <input id="email" type="email" v-model="email">
     <br><br>
     <label for="password">パスワード:</label>
-    <input id="password" type="password" v-model="name">
+    <input id="password" type="password" v-model="password">
     <br><br>
     <button @click="register">送信</button>
   
@@ -13,6 +13,8 @@
 </template>
 
 <script>
+import axios from '../axios-auth'
+
 export default {
   data () {
     return {
@@ -22,7 +24,18 @@ export default {
   },
   methods: {
       register () {
-          
+        axios.post(
+            '/accounts:signUp?key=AIzaSyC9xpsrA7B92sXf0ovzU7gSfk52YMIuL9U',
+            {
+                email: this.email,
+                password: this.password,
+                returnSecureToken: true
+            }
+        ).then(Response => {
+            console.log(Response)
+        }).catch(error => {
+            console.log(error.response.data.error)
+        })
       }
   }
 }
